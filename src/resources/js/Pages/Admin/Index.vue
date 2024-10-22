fc60e5949b797a2b0c47f2fd7b34579557963b7c
Sharif Khan <sharif.khan@mac.com>
2024-10-22 09:41:25 +0100

    v0.2.17

---
+++ src/resources/js/Pages/Admin/Index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <AdminLayout>
+        <template #title>Admin</template>
+        <ul class="text-white">
+            <li>
+                <Link href="/admin/blocks">Blocks</Link>
+            </li>
+            <li>
+                <Link href="/admin/pages">Pages</Link>
+            </li>
+        </ul>
+
+    </AdminLayout>
+</template>
